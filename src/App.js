2c3b1b206f2876a22008610804955bc650333c22
Blake Enloe <blakewenloe@yahoo.com>
2021-07-30 22:46:25 -0500

    setup hook for fetching launchpad data from SpaceX API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [launchPads, setData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios("https://api.spacexdata.com/v3/launchpads");
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
