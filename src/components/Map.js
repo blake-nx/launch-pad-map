acb1e7d97310bc047cc3fff3b4b8124e5248910d
Blake Enloe <blakewenloe@yahoo.com>
2021-07-30 23:09:46 -0500

    added launchpad data to marker popup

--- src/components/Map.js
+++ src/components/Map.js
@@ -22,7 +22,36 @@ export default function Map(props) {
         return (
           <Marker position={point} key={launchPad["id"]}>
             <Popup>
-              <p class="landpad-details"></p>
+              <p class="landpad-details">
+                Site Name:
+                <strong>
+                  {launchPad.site_name_long} ({launchPad.name})
+                </strong>
+                <br />
+                Status: <strong>{launchPad.status}</strong>
+                <br />
+                Attempted launches:
+                <strong>{launchPad.attempted_launches}</strong>
+                <br />
+                Successful launches:
+                <strong>{launchPad.successful_launches}</strong>
+                <br />
+                Vehicles Launched:
+                <ul>
+                  {launchPad.vehicles_launched.map((vehicle, i) => (
+                    <li key={i}>
+                      <strong>{vehicle}</strong>
+                    </li>
+                  ))}
+                </ul>
+                <p>
+                  <strong>Details</strong>: {launchPad.details}
+                </p>
+                Location:
+                <strong>
+                  {launchPad.location.name}, {launchPad.location.region}
+                </strong>
+              </p>
             </Popup>
           </Marker>
         );
